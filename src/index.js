19e52a13fd4dd1de879a17dd48e102b714461d2f
Choco <chocoarucan@gmail.com>
2023-05-01 22:18:35 +0800

    Added icon

--- src/index.js
+++ src/index.js
@@ -45,6 +45,13 @@ function displayWeather(response) {
   ).innerHTML = `${response.data.main.humidity}%`;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].main;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function showCity(city) {
